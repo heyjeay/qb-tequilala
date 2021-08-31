c926977f020620b942ea9a5c8fee8daf097c608b
LSRP <88177315+LifeStyleRolePlay@users.noreply.github.com>
2021-08-30 19:19:02 -0500

    Come rewrite of a the script
    Complete re write of the script to work with qb-target and nh-context

---
+++ config.lua
@@ -0,0 +1,99 @@
+Config = {}
+
+Config.Items = {
+    label = "Tequi-la-la Shop",
+    slots = 5,
+    items = {
+        [1] = {
+            name = "beer",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 1,
+        },
+        [2] = {
+            name = "whiskey",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 2,
+        },
+        [3] = {
+            name = "vodka",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 3,
+        },
+        [4] = {
+            name = "drink1",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 4,
+        },
+        [5] = {
+            name = "drink2",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 5,
+        },
+        [6] = {
+            name = "drink3",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 6,
+        },
+        [7] = {
+            name = "drink4",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 7,
+        },
+        [8] = {
+            name = "drink5",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 8,
+        },
+        [9] = {
+            name = "sandwich",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 9,
+        },
+        [10] = {
+            name = "tosti",
+            price = 3,
+            amount = 50,
+            info = {},
+            type = "item",
+            slot = 10,
+        },
+    }
+}
+
+Config.Locations = {
+    ["carspawn"] = {
+        label = "Car Spawn",
+        coords = {x =-574.33, y =268.94, z =82.45, h =85.29},
+    },
+}
+
+Config.Vehicles = {
+    ["patriot2"] = "Patriot Limo",
+}
